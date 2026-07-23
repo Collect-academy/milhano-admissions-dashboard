783ef9daf044f274aea624269c6bd1390881cab8
Collect-academy <contact@collect-academy.com>
2026-07-23 16:30:21 -0600

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-window.COLDEM_CONFIG = {
-  // Pega aquí la URL del Web App de Google Apps Script para compartir EOD entre Cinthia y Paty.
-  // Ejemplo: "https://script.google.com/macros/s/XXXXXXXX/exec"
-  API_URL: "",
-  SCHOOL_TIMEZONE: "America/Merida"
-};
